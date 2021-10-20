27bb56d3f06bb5905a126d158fd5504ae25933f7
yooml <ymlll0508@gmail.com>
2021-10-20 16:14:23 +0800

    feat: 🎸 add para_id field

--- src/mappings/mappingBalances.ts
+++ src/mappings/mappingBalances.ts
@@ -2,13 +2,17 @@ import { SubstrateExtrinsic, SubstrateEvent, SubstrateBlock } from "@subql/types
 import { BalancesTransfer } from "../types";
 import { Balance } from "@polkadot/types/interfaces";
 
-const MultiSignedAccount = ["1UbTddpy3RggGy3nk1vAc3msmSBasbhiYRQZnAdvNdUSXJn"]
+const MultiSignedAccount = [
+  { address: "1UbTddpy3RggGy3nk1vAc3msmSBasbhiYRQZnAdvNdUSXJn", para_id: 2000 },
+  { address: "148fP7zCq1JErXCy92PkNam4KZNcroG9zbbiPwMB1qehgeT4", para_id: 2001 }
+]
 
 export async function handleBalancesTransfer(event: SubstrateEvent): Promise<void> {
   const blockNumber = event.block.block.header.number.toNumber();
 
   const { event: { data: [from, to, balance] } } = event;
-  if (MultiSignedAccount.includes(to.toString())) {
+  const account = MultiSignedAccount.find(vendor => vendor.address === to.toString());
+  if (account !== undefined) {
     const record = new BalancesTransfer(blockNumber.toString() + '-' + event.idx.toString());
     record.block_height = blockNumber;
     record.event_id = event.idx;
@@ -17,6 +21,7 @@ export async function handleBalancesTransfer(event: SubstrateEvent): Promise<voi
     record.from = from.toString();
     record.to = to.toString();
     record.balance = (balance as Balance).toBigInt();
+    record.para_id = account.para_id;
     await record.save();
   }
 }
