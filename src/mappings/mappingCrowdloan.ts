9f7a1930e4fbab36a31c0a66668fc5c622373706
yooml <ymlll0508@gmail.com>
2021-11-03 20:37:08 +0800

    fix: 🐛 fix type of balaceOf

--- src/mappings/mappingCrowdloan.ts
+++ src/mappings/mappingCrowdloan.ts
@@ -1,9 +1,9 @@
 import { SubstrateEvent } from "@subql/types";
+import { SignedBlock, Balance } from "@polkadot/types/interfaces";
 import { Contributed } from "../types/models";
 
 export async function handleContributed(event: SubstrateEvent): Promise<void> {
   const { event: { data: [account_id_origin, para_id_origin, balance_origin] } } = event;
-  const balance = balance_origin.toString();
   const account_id = account_id_origin.toString();
   const para_id = para_id_origin.toString();
   const blockNumber = event.extrinsic.block.block.header.number.toNumber();
@@ -14,6 +14,6 @@ export async function handleContributed(event: SubstrateEvent): Promise<void> {
   entity.time = event.block.timestamp;
   entity.accountId = account_id;
   entity.paraId = para_id;
-  entity.balanceOf = balance;
+  entity.balanceOf = (balance_origin as Balance).toBigInt();
   await entity.save();
 }
