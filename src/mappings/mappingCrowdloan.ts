93ac684f1b21c303e039743235965bf1b14b9f21
yooml <ymlll0508@gmail.com>
2021-11-04 19:11:53 +0800

    fix: 🐛 fix type of Contributed

--- src/mappings/mappingCrowdloan.ts
+++ src/mappings/mappingCrowdloan.ts
@@ -1,19 +1,22 @@
 import { SubstrateEvent } from "@subql/types";
 import { SignedBlock, Balance } from "@polkadot/types/interfaces";
+import { Compact } from '@polkadot/types';
+import type { ParaId } from '@polkadot/types/interfaces/parachains';
 import { Contributed } from "../types/models";
 
 export async function handleContributed(event: SubstrateEvent): Promise<void> {
   const { event: { data: [account_id_origin, para_id_origin, balance_origin] } } = event;
+  const balance = (balance_origin as Compact<Balance>).toBigInt();
   const account_id = account_id_origin.toString();
-  const para_id = para_id_origin.toString();
-  const blockNumber = event.extrinsic.block.block.header.number.toNumber();
+  const para_id = (para_id_origin as ParaId).toNumber();
+  const blockNumber = event.block.block.header.number.toNumber();
   const entity = new Contributed(blockNumber.toString() + '-' + event.idx.toString());
   entity.blockHeight = blockNumber;
   entity.eventId = event.idx;
   entity.extrinsicId = event.extrinsic.idx;
-  entity.time = event.block.timestamp;
+  entity.block_timestamp = event.block.timestamp;
   entity.accountId = account_id;
   entity.paraId = para_id;
-  entity.balanceOf = (balance_origin as Balance).toBigInt();
+  entity.balanceOf = balance;
   await entity.save();
 }
