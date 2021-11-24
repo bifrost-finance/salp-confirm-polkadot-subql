08fc76ad6d02fbbaae2bcf563f333b4db4f479cd
yooml <ymlll0508@gmail.com>
2021-11-24 15:21:07 +0800

    feat: 🎸 add ContributedBatch

--- src/mappings/mappingBalances.ts
+++ src/mappings/mappingBalances.ts
@@ -1,7 +1,8 @@
 import { SubstrateExtrinsic, SubstrateEvent, SubstrateBlock } from "@subql/types";
-import { TransferBatch } from "../types";
-import { Bytes } from "@polkadot/types";
+import { TransferBatch, Contributed, ContributedBatch } from "../types";
+import { Bytes, Compact } from "@polkadot/types";
 import { SignedBlock, Balance } from "@polkadot/types/interfaces";
+import type { ParaId } from '@polkadot/types/interfaces/parachains';
 
 const MultiSignedAccount = [
   { address: "14AMZ3gw4tRsrdp78i4MmHZ8EFbXTMfuXGQMEC3t1GoqLboH" }
@@ -41,9 +42,9 @@ export async function handleBalancesTransfer(event: SubstrateEvent): Promise<voi
       if (index == 2 && value.callIndex.toString() === SystemRemarkWithEventCallId) {
         record.referrer = hex_to_ascii((value.args.remark as Bytes).toString().slice(2));
       }
-      // if (index == 3 && value.callIndex.toString() === SystemRemarkWithEventCallId) {
-      //   record.eth_address = hex_to_ascii((value.args.remark as Bytes).toString().slice(2));
-      // }
+      if (index == 3 && value.callIndex.toString() === SystemRemarkWithEventCallId) {
+        record.eth_address = hex_to_ascii((value.args.remark as Bytes).toString().slice(2));
+      }
     })
     await record.save();
   }
@@ -56,4 +57,36 @@ function hex_to_ascii(str1) {
     str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
   }
   return str;
+}
+
+export async function handleContributed2(event: SubstrateEvent): Promise<void> {
+  const blockNumber = event.block.block.header.number.toNumber();
+
+  const { event: { data: [account_id_origin, para_id_origin, balance_origin] } } = event;
+  const balance = (balance_origin as Compact<Balance>).toBigInt();
+  const account_id = account_id_origin.toString();
+  const para_id = (para_id_origin as ParaId).toNumber();
+  const account = MultiSignedAccount.find(vendor => vendor.address === account_id);
+  const tx = event.extrinsic.extrinsic.method;
+  if (tx !== undefined && tx.callIndex.toString() === BatchAllCallId && account !== undefined) {
+    const record = new ContributedBatch(blockNumber.toString() + '-' + event.idx.toString());
+    record.block_height = blockNumber;
+    record.block_hash = event.block.hash ? event.block.hash.toString() : null;
+    record.event_id = event.idx;
+    record.extrinsic_id = event.extrinsic ? event.extrinsic.idx : null;
+    record.extrinsic_hash = event.extrinsic ? event.extrinsic.extrinsic.hash.toString() : null;
+    record.block_timestamp = event.block.timestamp;
+    record.accountId = account_id;
+    record.paraId = para_id;
+    record.balanceOf = balance;
+
+    const args = JSON.parse(JSON.stringify(tx.args));
+    logger.info(JSON.stringify(tx.args))
+    args[0].forEach((value, index) => {
+      if (index == 2 && value.callIndex.toString() === SystemRemarkWithEventCallId) {
+        record.account = hex_to_ascii((value.args.remark as Bytes).toString().slice(2));
+      }
+    })
+    await record.save();
+  }
 }
